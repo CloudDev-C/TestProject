61ba0b7cb66d871c56b37b9b64a981f7218a0d70
Турчинский Владислав <turchinsky_v@od.itstep.local>
2021-10-13 18:31:35 +0300

    add func for show array

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,8 +3,22 @@
 
 #include <iostream>
 
+using namespace std;
+
+void show_array(const int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	cout << endl;
+}
+
 int main()
 {
-    std::cout << "Hello World!\n";
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+
+	show_array(arr, 10);
 }
 
