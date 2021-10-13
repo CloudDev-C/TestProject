8d7150266e9211d2a137439e8a6ff9edde5b5c60
CloudDev-C <54555643+CloudDev-C@users.noreply.github.com>
2021-10-13 18:40:40 +0300

    Add func init array (#1)
    Co-authored-by: Турчинский Владислав <turchinsky_v@od.itstep.local>

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -15,9 +15,21 @@ void show_array(const int* arr, int size)
 	cout << endl;
 }
 
+void init_array(int *arr, int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+}
+
 int main()
 {
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	srand(time(NULL));
+
+	int arr[10];
+
+	init_array(arr, 10);
 
 	show_array(arr, 10);
 }
